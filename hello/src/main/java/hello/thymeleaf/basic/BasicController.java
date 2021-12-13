c1eb0162967c76426f1707205e36f72035e6e4a7
kimun kim <kkm97351@gmail.com>
2021-12-13 19:09:43 +0900

    주석, 블록

--- hello/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ hello/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import javax.swing.plaf.multi.MultiOptionPaneUI;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -99,6 +100,18 @@ public class BasicController {
     public String condition(Model model) {
         addUsers(model);
         return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
     }
 
     private void addUsers(Model model) {
